7616c31e7cdb90a17b064d7bde1e969a1f9008fe
Nicholas Bourey <ntbourey@me.com>
2023-12-30 06:06:45 -0800

    Check Party Floor Room Count (#91)
    Check that the total number of applications (Not Denied, Deleted or Old) against the Event’s max hosts number (plus 5 for a waitlist) and disable taking new applications.

--- partyfloor/views.py
+++ partyfloor/views.py
@@ -1,4 +1,4 @@
-from core.models import get_current_event, DaysAvailable
+from core.models import get_current_event, ApplicationState, DaysAvailable
 from django.conf import settings
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
@@ -9,16 +9,30 @@ from modules.email import send_paw_email
 from .forms import HostForm
 from .models import Event, PartyHost
 
+def __is_partyfloor_full():
+    max_partyhosts = 0
+    partyhosts_count = 0
+    event = get_current_event()
+    if (event):
+        max_partyhosts = event.max_party_rooms + 5
+        partyhosts_count = PartyHost.objects.filter(event=event).exclude(host_state=[ApplicationState.STATE_DENIED, ApplicationState.STATE_DELETED, ApplicationState.STATE_OLD]).count()
+
+    return (partyhosts_count >= max_partyhosts)
+
 # Create your views here.
 def index(request):
     event = get_current_event()
     context =  {
         'is_partyfloor': True,
+        'is_partyfloor_full': __is_partyfloor_full(),
         'event': event,
     }
     return render(request, 'partyfloor.html', context)
 
 def apply(request):
+    if (__is_partyfloor_full()):
+        return HttpResponseRedirect(reverse('partyfloor:index'))
+    
     event = get_current_event()
     form = HostForm()
     context =  {
