a71f80033c1f47346491297e8f09582508530b81
Nicholas Bourey <ntbourey@me.com>
2024-02-11 13:49:19 -0800

    Small Fixes
    Small Fixes for templates.

--- console/views_new/console.py
+++ console/views_new/console.py
@@ -49,20 +49,30 @@ class ConsoleLoginPageView(PageView):
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     template_name = 'console-password-reset.html'
     email_template_name = 'console-email-reset-password.html'
+    html_email_template_name = 'console-email-reset-password.html'
     subject_template_name = 'console-email-reset-password.txt'
     success_message = "We've emailed you instructions for setting your password, " \
                       "if an account exists with the email you entered. You should receive them shortly." \
                       " If you don't receive an email, " \
                       "please make sure you've entered the address you registered with, and check your spam folder."
     success_url = reverse_lazy('console:index')
+    extra_context = {
+        'event': get_current_event()
+    }
 
 class ResetPasswordConfirmView(PasswordResetConfirmView):
     template_name = 'console-password-reset-confirm.html'
     success_url = reverse_lazy('console:forgot-password-complete')
+    extra_context = {
+        'event': get_current_event()
+    }
     
 
 class ResetPasswordCompelteView(PasswordResetCompleteView):
     template_name = 'console-password-reset-complete.html'
+    extra_context = {
+        'event': get_current_event()
+    }
 
 class ConsoleLogoutRedirect(View):
     def get(self, request, *args, **kwargs):
