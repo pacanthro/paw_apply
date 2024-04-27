9e0e3339aa0c55b15a0714f36e0ed67e40fa4bb0
Nicholas Bourey <ntbourey@me.com>
2024-04-27 06:38:53 -0700

    Gh30 Performer Scheduling (#142)
    * Performance Scheduling
    
    Added support for viewing and scheduling performances.
    
    * Allow Unscheduling
    
    Allow a performer to get unscheduled.
    
    * Small Tweaks
    
    Add assign and unschedule functionality to Performer Detail page. Also fixed some jarring redirect behavior.
    
    * Fix Layout
    
    Fixed Unscheduled performers layout.

--- console/forms/__init__.py
+++ console/forms/__init__.py
@@ -1,3 +1,4 @@
 from .authentication import LoginForm
 from .merchants import MerchantAssignTableForm
 from .partyfloor import HostAssignRoomForm
+from .performers import PerformerScheduleDayForm, PerformerScheduleSlotForm
