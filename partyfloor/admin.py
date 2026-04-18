7dab2411fa13df01094823f7c8a54fe1a09ddd69
Nicholas Bourey <ntbourey@me.com>
2026-04-18 06:31:20 -0700

    Register Content with Admin Site (#252)
    Register Content with Admin Site so it can always be edited.

--- partyfloor/admin.py
+++ partyfloor/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PartyHost
+from .models import PartyHost, PartyHostContent
 
 # Register your models here.
 class PartyHostAdmin(admin.ModelAdmin):
     list_display = ('event', 'email', 'legal_name', 'fan_name')
     list_filter = ['event']
 
+class PartyHostContentAdmin(admin.ModelAdmin):
+    list_display = ('card_title', 'card_body', 'card_cta')
+
 admin.site.register(PartyHost, PartyHostAdmin)
+admin.site.register(PartyHostContent, PartyHostContentAdmin)
--- volunteers/admin.py
+++ volunteers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import TimesAvailable, Volunteer, VolunteerTask
+from .models import TimesAvailable, Volunteer, VolunteerContent, VolunteerTask
 
 class TimesAvailableAdmin(admin.ModelAdmin):
     list_display = ('name', 'order')
@@ -10,10 +10,14 @@ class VolunteerAdmin(admin.ModelAdmin):
     list_display = ('event', 'email', 'legal_name', 'fan_name')
     list_filter = ['event']
 
+class VolunteerContentAdmin(admin.ModelAdmin):
+    list_display = ('card_title', 'card_body', 'card_cta')
+
 class VolunteerTaskAdmin(admin.ModelAdmin):
     list_display = ('event', 'volunteer', 'task_name')
 
 # Register your models here.
 admin.site.register(TimesAvailable, TimesAvailableAdmin)
 admin.site.register(Volunteer, VolunteerAdmin)
+admin.site.register(VolunteerContent, VolunteerContentAdmin)
 admin.site.register(VolunteerTask, VolunteerTaskAdmin)
