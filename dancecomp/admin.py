63227ac540c22d3575b5932f8ceb66351388fbc5
Nicholas Bourey <ntbourey@me.com>
2026-04-25 08:20:15 -0700

    CompetitorContent Admin
    Add CompetitorContent to the admin site.

--- dancecomp/admin.py
+++ dancecomp/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Competitor
+from .models import Competitor, CompetitorContent
 
 # Register your models here.
 class CompetitorAdmin(admin.ModelAdmin):
     list_display = ('event', 'email', 'legal_name', 'fan_name')
     list_filter = ['event']
 
+class CompetitorContentAdmin(admin.ModelAdmin):
+    list_display = ('card_title', 'card_body', 'card_cta')
+
 admin.site.register(Competitor, CompetitorAdmin)
+admin.site.register(CompetitorContent, CompetitorContentAdmin)
